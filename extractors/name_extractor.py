74072e34f38f936c06d9c356eb5a16f2f31543b6
dronperminov <dronperminov@gmail.com>
2020-11-12 22:44:56 +0300

    optimize name extractor and update readme

--- extractors/name_extractor.py
+++ extractors/name_extractor.py
@@ -1,30 +1,42 @@
 import re
-from constants import *
 from normalizers.name_normalizer import NameNormalizer
-from typing import List, Tuple
 
 
 class NameExtractor:
     def __init__(self, for_metric=True):
-        self.strip_chars = " \n':|-©‚`" + ("\"" if for_metric else "")
-        self.name_normalizer = NameNormalizer(self.strip_chars)
+        self.strip_chars = " \n':|-©‚`"
+        self.lstrip_chars = "-"
+        self.name_normalizer = NameNormalizer(self.strip_chars + ("\"" if for_metric else ""))
         self.paragraph_regexp = re.compile(r"(?:.*\n)+?\n", re.M)
 
         self.name_regexps = [
-            re.compile(r"^О[бБ]? .+$", re.M),
+            re.compile(r"^Г?О[бБ]? .+$", re.M),
             re.compile(r"^Отдельные .+$", re.M),
             re.compile(r"^Вопросы .+$", re.M),
+            re.compile(r"по делу о.*$", re.M),
             re.compile(r"\bО[бБ]? .+$", re.M),
         ]
 
     def clear_paragraph(self, paragraph: str) -> str:
+        lines = [line.lstrip(self.lstrip_chars) for line in paragraph.splitlines() if not re.fullmatch(r"О(?: \d+)*", line)]
+        paragraph = "\n".join(lines)
+
         return paragraph.strip(self.strip_chars).replace("\n", " ")
+
+    def can_join_paragraphs(self, paragraph1: str, paragraph2: str) -> bool:
+        if paragraph1.lower().endswith("ого") or paragraph1.endswith(","):
+            return True
+
+        if re.search(r"^(?:на |в |об? |\"Об? |Республики )", paragraph2):
+            return True
+
+        return False
 
     def extract(self, text: str) -> str:
         paragraphs = [self.clear_paragraph(paragraph) for paragraph in self.paragraph_regexp.findall(text + "\n")]
 
         for i, paragraph in enumerate(paragraphs[:-1]):
-            if paragraph.lower().endswith("ого") or paragraph.endswith(","):
+            if self.can_join_paragraphs(paragraph, paragraphs[i + 1]):
                 paragraphs[i] += " " + paragraphs[i + 1]
                 paragraphs[i + 1] = ""
 
--- normalizers/name_normalizer.py
+++ normalizers/name_normalizer.py
@@ -9,28 +9,32 @@ class NameNormalizer:
 
         self.replace_regexps = [
             (re.compile("'"), "\""),
-            (re.compile("[|=*]"), ""),
+            (re.compile("[|=*‘]"), ""),
             (re.compile("_| [,‚]"), " "),
             (re.compile("©"), "с"),
+            (re.compile("^ГО "), "О "),
             (re.compile("ОЁ"), "Об"),
-            (re.compile("Вв"), "в")
+            (re.compile("Вв"), "в"),
+            (re.compile("-3С"), "-ЗС"),
+            (re.compile("—"), "-")
         ]
 
     def normalize(self, name: str) -> str:
         for regexp, replacement in self.replace_regexps:
             name = regexp.sub(replacement, name)
 
-        name = name.strip(self.strip_chars)
-
-        if name.startswith("О законе") or name.startswith("О Законе") and " \"О" in name:
+        if (name.startswith("О законе") or name.startswith("О Законе")) and " \"О" in name:
             name = name[name.index(" \"О")+2:]
+        elif name.startswith("по делу о") and re.search(r"\".*\"", name):
+            name = re.findall(r"\"(.*)\"", name)[-1]
+
+        name = name.strip(self.strip_chars)
 
         for word in self.stop_words:
             if word in name:
                 name = name[:name.index(word)]
 
-        while "  " in name:
-            name = name.replace("  ", " ")
+        name = re.sub(r" +", " ", name)
 
         if re.fullmatch(r".* [.уоО]|.*\".", name):
             name = name[:-2]
